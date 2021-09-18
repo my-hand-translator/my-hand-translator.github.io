d777759a5ab7e59873f99f1c9dba40f64108c121
suhyeonglee <sounmind777@gmail.com>
2021-09-18 19:57:01 +0900

    docs: OAuth 인증 관련 설명 수정

--- src/pages/GetStartedPage.jsx
+++ src/pages/GetStartedPage.jsx
@@ -30,11 +30,10 @@ function GetStartedPage() {
 
       <Container>
         <h2>회원 가입 후 사전준비 과정</h2>
-
         <ul>
           <li>
             <ListContainer>
-              <p>1. 프로젝트 생성 하기</p>
+              <h3>1. 프로젝트 생성 하기</h3>
               <img
                 srcSet="images/getStarted/1_create_project_1200.png 1200w,
                   images/getStarted/1_create_project_768.png 768w"
@@ -45,10 +44,10 @@ function GetStartedPage() {
           </li>
           <li>
             <ListContainer>
-              <p>
-                2. API 및 서비스 - 대시보드 - 상단에 API 및 서비스 사용 및 설정 - Cloud Translator
+              <h3>
+                2. API 및 서비스 - 대시보드 - 상단에 API 및 서비스 사용 및 설정 - Cloud Translation
                 API 사용
-              </p>
+              </h3>
               <img
                 srcSet="images/getStarted/2_1_create_api_1200.png 1200w,
                   images/getStarted/2_1_create_api_768.png 768w"
@@ -65,10 +64,10 @@ function GetStartedPage() {
           </li>
           <li>
             <ListContainer>
-              <p>
+              <h3>
                 3. OAuth 동의 화면 구성 - 외부 선택 - 앱 이름과, 사용자 지원 이메일, 개발자 연락처
-                정보 입력
-              </p>
+                정보 입력, 아래 저장 버튼 클릭
+              </h3>
               <img
                 srcSet="images/getStarted/3_1_create_OAuth_1200.png 1200w,
                   images/getStarted/3_1_create_OAuth_768.png 768w"
@@ -87,15 +86,8 @@ function GetStartedPage() {
                 src="images/getStarted/3_3_create_OAuth_1200.png"
                 alt="3_3_create_OAuth"
               />
-              <ListContainer
-                css={{
-                  wordBreak: "break-all",
-                }}
-              >
-                <p>
-                  - 승인된 리디렉션 URI에 https://ebkndhbalobmkeeajcpcgolcmklomoca.chromiumapp.org
-                  입력
-                </p>
+              <ListContainer>
+                <p>OAuth 동의 화면에서 앱 게시 클릭 </p>
                 <img
                   srcSet="images/getStarted/3_4_create_OAuth_1200.png 1200w,
                   images/getStarted/3_4_create_OAuth_768.png 768w"
@@ -103,17 +95,51 @@ function GetStartedPage() {
                   alt="3_4_create_OAuth"
                 />
               </ListContainer>
+            </ListContainer>
+          </li>
+          <li>
+            <ListContainer>
+              <h2> OAuth 클라이언트 ID 만들기 </h2>
+              <img src="images/getStarted/3_5_create_OAuth.png" alt="3_5_create_OAuth" />
+            </ListContainer>
+            <ListContainer>
+              <p>어플리케이션 유형으로 크롬 앱 선택하기 </p>
+              <img src="images/getStarted/3_6_create_OAuth.png" alt="3_6_create_OAuth" />
+            </ListContainer>
+            <ListContainer>
+              <p>애플리케이션 ID에 확장프로그램 ID 작성해 만들기 </p>
+              <img
+                srcSet="images/getStarted/3_7_create_OAuth.png"
+                src="images/getStarted/3_7_create_OAuth.png"
+                alt="3_6_create_OAuth"
+              />
+            </ListContainer>
+            <ListContainer>
+              <p>Oauth2의 사용자 인증 정보와 프로젝트 아이디를 복사합니다. </p>
+              <img src="images/getStarted/3_8_create_OAuth.png" alt="3_8_create_OAuth" />
+            </ListContainer>
+          </li>
+          <li>
+            <ListContainer>
+              <h3>
+                4. 확장프로그램이 설치되어 있는 경로의 manifest.json 파일에 클라이언트 ID와 프로젝트
+                ID를 붙여넣습니다.
+              </h3>
+              <img src="images/getStarted/3_9_create_OAuth.png" alt="3_9_create_OAuth" />
               <ListContainer>
-                <p>- OAuth 동의 화면에서 앱 게시 클릭 </p>
-                <img
-                  srcSet="images/getStarted/3_5_create_OAuth_1200.png 1200w,
-                  images/getStarted/3_5_create_OAuth_768.png 768w"
-                  src="images/getStarted/3_5_create_OAuth_1200.png"
-                  alt="3_5_create_OAuth"
-                />
+                <p>Google 로그인 후 권한 동의를 합니다.</p>
               </ListContainer>
             </ListContainer>
           </li>
+          <li>
+            <ListContainer>
+              <h3>
+                5. 브라우저의 확장 프로그램 아이콘을 클릭하면, 구글 OAuth2 인증이 시작될 것입니다.
+              </h3>
+            </ListContainer>
+          </li>
+          <li />
+          <li />
         </ul>
       </Container>
     </Main>
